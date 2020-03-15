d025246596493cdeda733df16d49b3826d7faacb
JaesunJin <60768423+JaesunJin@users.noreply.github.com>
2020-03-15 18:12:04 +0900

    Add files via upload

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/labstack/echo"
+)
+
+func handleHome(c echo.Context) error {
+	return c.File("home.html")
+}
+
+func handleScrape(c echo.Context) error {
+	return c.File("home.html")
+}
+
+func main() {
+	e := echo.New()
+	e.GET("/", handleHome)
+	e.POST("/scrape", handleScrape)
+	e.Logger.Fatal(e.Start(":1986"))
+}
